30c5c17235ff8b727858c89d231992e208c565c7
Rafael Laurindo Cachoeira <rafaelcachoeira@outlook.com>
2023-02-28 15:44:26 -0300

    fix empty deps

--- src/hooks/use-form-success-resolver.ts
+++ src/hooks/use-form-success-resolver.ts
@@ -4,5 +4,5 @@ import { useEffect } from 'react';
 export const useFormSuccessResolver = <TInput, TOutput>(resolver: (input: TInput, output: TOutput) => void) => {
     const control = useFormControl();
 
-    useEffect(() => control.addSuccessResolver(resolver), []);
+    useEffect(() => control.addSuccessResolver(resolver), [resolver]);
 };
